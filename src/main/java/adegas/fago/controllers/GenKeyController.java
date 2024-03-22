7fe4580bc340d328404ce779ba1b47ba83285bfb
Victor Vargas <victor.andresv@gmail.com>
2024-03-22 15:08:36 -0300

    jwt and qrcode url

--- src/main/java/adegas/fago/controllers/GenKeyController.java
+++ src/main/java/adegas/fago/controllers/GenKeyController.java
@@ -1,5 +1,6 @@
 package adegas.fago.controllers;
 
+import adegas.fago.helpers.JwtHelper;
 import adegas.fago.helpers.QRCodesHelper;
 import adegas.fago.interfaces.UserRepository;
 import adegas.fago.models.GenKeyDto;
@@ -21,37 +22,80 @@ public class GenKeyController {
     @Autowired
     private UserRepository repository;
 
-    @GetMapping("/gen-key/{companyId}/{phone}")
-    public ResponseEntity<GenKeyDto> Generate(@PathVariable String companyId, @PathVariable String phone){
+    @GetMapping("/access-token/{companyId}/{phone}")
+    public ResponseEntity<GenKeyDto> AccessToken(@PathVariable String companyId, @PathVariable String phone){
 
         if(!companyId.isEmpty() && !phone.isEmpty()){
             UserCollection user = repository.findByCompanyIdAndPhone(companyId, phone);
 
-            String key = "";
+            String publicKey = "";
+            String privateKey = "";
             if(user.getPublicKey() != null){
-                key = user.getPublicKey();
+                publicKey = user.getPublicKey();
+                privateKey = user.getPrivateKey();
             } else {
                 try {
                     KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
                     keyPairGen.initialize(2048);
                     KeyPair pair = keyPairGen.generateKeyPair();
                     Base64.Encoder encoder = Base64.getEncoder();
-                    String publicKey = encoder.encodeToString(pair.getPublic().getEncoded());
-                    String privateKey = encoder.encodeToString(pair.getPrivate().getEncoded());
+                    publicKey = encoder.encodeToString(pair.getPublic().getEncoded());
+                    privateKey = encoder.encodeToString(pair.getPrivate().getEncoded());
 
                     user.setPrivateKey(privateKey);
                     user.setPublicKey(publicKey);
 
                     repository.save(user);
-
-                    key = publicKey;
                 } catch (Exception ignored) { }
             }
-            if(!key.isEmpty()){
-                String base64Image = QRCodesHelper.GenerateAztec(key);
-                GenKeyDto GenKeyDto = new GenKeyDto();
-                GenKeyDto.setQrCode(base64Image);
-                return new ResponseEntity<>(GenKeyDto, HttpStatus.OK);
+            if(!publicKey.isEmpty() && !privateKey.isEmpty()){
+
+                JwtHelper jwtHelper = new JwtHelper(user.getID(), publicKey, privateKey);
+                GenKeyDto genKeyDto = new GenKeyDto();
+                genKeyDto.setJwt(jwtHelper.GetToken());
+                return new ResponseEntity<>(genKeyDto, HttpStatus.OK);
+            }
+
+        }
+
+        return new ResponseEntity<>(new GenKeyDto(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/access-qr-code/{companyId}/{phone}")
+    public ResponseEntity<GenKeyDto> AccessQrCode(@PathVariable String companyId, @PathVariable String phone){
+
+        if(!companyId.isEmpty() && !phone.isEmpty()){
+            UserCollection user = repository.findByCompanyIdAndPhone(companyId, phone);
+
+            String publicKey = "";
+            String privateKey = "";
+            if(user.getPublicKey() != null){
+                publicKey = user.getPublicKey();
+                privateKey = user.getPrivateKey();
+            } else {
+                try {
+                    KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+                    keyPairGen.initialize(2048);
+                    KeyPair pair = keyPairGen.generateKeyPair();
+                    Base64.Encoder encoder = Base64.getEncoder();
+                    publicKey = encoder.encodeToString(pair.getPublic().getEncoded());
+                    privateKey = encoder.encodeToString(pair.getPrivate().getEncoded());
+
+                    user.setPrivateKey(privateKey);
+                    user.setPublicKey(publicKey);
+
+                    repository.save(user);
+                } catch (Exception ignored) { }
+            }
+            if(!publicKey.isEmpty() && !privateKey.isEmpty()){
+
+                JwtHelper jwtHelper = new JwtHelper(user.getID(), publicKey, privateKey);
+
+                String base64Image = QRCodesHelper.GenerateAztec(jwtHelper.GetToken());
+                GenKeyDto genKeyDto = new GenKeyDto();
+                genKeyDto.setQrCode(base64Image);
+                return new ResponseEntity<>(genKeyDto, HttpStatus.OK);
             }
 
         }
--- src/main/java/adegas/fago/models/GenKeyDto.java
+++ src/main/java/adegas/fago/models/GenKeyDto.java
@@ -2,6 +2,15 @@ package adegas.fago.models;
 
 public class GenKeyDto {
     private String qrCode;
+    private String jwt;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
 
     public String getQrCode() {
         return qrCode;
