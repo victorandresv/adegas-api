87627823ade2a55bdc324c157c77342f5d533eb9
Victor Vargas <victor.andresv@gmail.com>
2024-03-23 17:01:28 -0300

    Response model

---
+++ src/main/java/adegas/fago/controllers/ResponseModel.java
@@ -0,0 +1,31 @@
+package adegas.fago.controllers;
+
+public class ResponseModel {
+    private boolean success;
+    private Object data;
+    private String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
