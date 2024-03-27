5a3fbe8e4a7677ef65714eedfebac60300044076
Victor Vargas <victor.andresv@gmail.com>
2024-03-27 14:03:06 -0300

    order creation

--- src/main/java/adegas/fago/controllers/OrdersController.java
+++ src/main/java/adegas/fago/controllers/OrdersController.java
@@ -7,10 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class OrdersController {
     @Autowired
     private OrdersRepository repository;
+
 
     @PostMapping(value="/orders")
     public ResponseEntity<OrderCollection> Create(@RequestBody OrderCollection payload){
