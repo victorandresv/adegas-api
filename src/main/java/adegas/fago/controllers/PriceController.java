279f3972c35b3bbce76a093078b9cd1fcae5640d
Victor Vargas <victor.andresv@gmail.com>
2024-03-27 20:02:13 -0300

    Get last price

--- src/main/java/adegas/fago/controllers/PriceController.java
+++ src/main/java/adegas/fago/controllers/PriceController.java
@@ -1,19 +1,30 @@
 package adegas.fago.controllers;
 
+import adegas.fago.helpers.GenKeyHelper;
+import adegas.fago.helpers.HeadersHelper;
+import adegas.fago.interfaces.KeysRepository;
 import adegas.fago.interfaces.PricesRepository;
+import adegas.fago.interfaces.UserRepository;
 import adegas.fago.models.PriceCollection;
+import adegas.fago.models.UserCollection;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PriceController {
 
     @Autowired
     private PricesRepository repository;
+    @Autowired
+    private KeysRepository keyRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping(value="/prices")
     public ResponseEntity<PriceCollection> Create(@RequestBody PriceCollection payload){
@@ -25,5 +36,20 @@ public class PriceController {
     public ResponseEntity<List<PriceCollection>> Find(@PathVariable String companyId){
         List<PriceCollection> list = repository.findByCompanyId(companyId);
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/price/current")
+    public ResponseEntity<PriceCollection> Find(@RequestHeader Map<String, String> headers){
+        String jwt = HeadersHelper.GetAccessTokenHeader(headers);
+        JSONObject jsonObject = GenKeyHelper.VerifyJsonWebToken(jwt, keyRepository, false);
+        if(jsonObject == null){
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+        UserCollection user = userRepository.findOneById(jsonObject.getString("oid"));
+        if(user == null){
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+        PriceCollection priceCollection = repository.findCurrentByCompanyId(user.getCompanyId());
+        return new ResponseEntity<>(priceCollection, HttpStatus.OK);
     }
 }
--- src/main/java/adegas/fago/interfaces/PricesRepository.java
+++ src/main/java/adegas/fago/interfaces/PricesRepository.java
@@ -1,6 +1,7 @@
 package adegas.fago.interfaces;
 
 import adegas.fago.models.PriceCollection;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -9,4 +10,11 @@ import java.util.List;
 public interface PricesRepository extends MongoRepository<PriceCollection, String> {
     @Query(value = "{'companyId': ?0}", sort = "{'timestamp': -1}}")
     List<PriceCollection> findByCompanyId(String companyId);
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'companyId' : ?0 } }",
+            "{ '$sort' : { 'timestamp' : -1 } }",
+            "{ '$limit' : 1 }"
+    })
+    PriceCollection findCurrentByCompanyId(String companyId);
 }
