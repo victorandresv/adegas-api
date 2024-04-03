86f0c314c54374438a5137b88850d3214578ae5b
Victor Vargas <victor.andresv@gmail.com>
2024-04-03 08:15:13 -0300

    Orders location

--- src/main/java/adegas/fago/interfaces/OrdersLocationRepository.java
+++ src/main/java/adegas/fago/interfaces/OrdersLocationRepository.java
@@ -3,7 +3,6 @@ package adegas.fago.interfaces;
 import adegas.fago.models.OrdersLocationCollection;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
@@ -11,7 +10,7 @@ public interface OrdersLocationRepository extends MongoRepository<OrdersLocation
     @Aggregation(pipeline = {
             "{ '$match': { 'orderId' : ?0 } }",
             "{ '$sort' : { 'timeStamp' : 1 } }",
-            "{ '$limit' : 20 }"
+            "{ '$limit' : 100 }"
     })
     List<OrdersLocationCollection> findLocationsByOrderId(String orderId);
 }
